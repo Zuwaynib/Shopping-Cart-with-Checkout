15d5db356e5cebe91d6b1d77a5582a3576bdaa5e
Zuwaynib <zainabadeoje@gmail.com>
2025-03-15 22:37:12 +0100

    product.js: created a .js file to store product details

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// Sample products with image URLs
-const products = [
-    { 
-        id: 1, 
-        name: 'Shirt', 
-        price: 19.99, 
-        image: 'https://picsum.photos/200/200?random=1' 
-    },
-    { 
-        id: 2, 
-        name: 'Pants', 
-        price: 29.99, 
-        image: 'https://picsum.photos/200/200?random=2' 
-    },
-    { 
-        id: 3, 
-        name: 'Shoes', 
-        price: 49.99, 
-        image: 'https://picsum.photos/200/200?random=3' 
-    }
-];
-
 let cart = [];
 
 document.addEventListener('DOMContentLoaded', () => {
---
+++ products.js
@@ -0,0 +1,21 @@
+// Products with images
+const products = [
+    { 
+        id: 1, 
+        name: 'Shirt', 
+        price: 19.99, 
+        image: 'https://picsum.photos/200/200?random=1' 
+    },
+    { 
+        id: 2, 
+        name: 'Pants', 
+        price: 29.99, 
+        image: 'https://picsum.photos/200/200?random=2' 
+    },
+    { 
+        id: 3, 
+        name: 'Shoes', 
+        price: 49.99, 
+        image: 'https://picsum.photos/200/200?random=3' 
+    }
+];
